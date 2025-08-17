1621f13093ea947f9fa01cf89e68756e8686dd1d
unknown <puneetsingh91100@gmail.com>
2025-08-17 23:15:01 +0530

    cv commit

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import cv from '../../assets/cv.pdf'
+import resume from '../../assets/Resume.pdf'
 import { FaRegMessage } from "react-icons/fa6";
 
 // import '../../index'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a className='btn2' href={cv} download >Download Catalog</a>
+      <a className='btn2' href={resume} download > Resume</a>
+      <a className='btn2' href={cv} download > Catalog</a>
       <a href="#contact" className='btn3'><FaRegMessage/></a>
       
     </div>
