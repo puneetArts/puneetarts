18ed75bf81c153bceb172d2d184244f9d8432813
Puneet Singh <puneetsingh91100@gmail.com>
2025-11-22 12:02:38 +0530

    Projects

--- src/components/DSA/Dsa.jsx
+++ src/components/DSA/Dsa.jsx
@@ -30,7 +30,7 @@ const Dsa = () => {
 
   // fallback for if API fails
   const leetFallback = {
-    totalSolved: '120+',
+    totalSolved: '140+',
     easySolved: 40,
     mediumSolved: 45,
     hardSolved: 15,
